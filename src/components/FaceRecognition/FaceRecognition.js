d3fb407e741620c2298a88c38700188c18d7cfc4
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-08-02 22:04:31 -0700

    Applying for mutliple images

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
+  console.log(boxes)
   return (
     <div className="center ma">
       <div className="absolute mt2">
