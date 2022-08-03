5749915192dcf1ac6e794ec7d69272f0f6a68899
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-08-02 20:53:01 -0700

    Applting for multiple images

--- src/App.js
+++ src/App.js
@@ -43,8 +43,10 @@ class App extends Component {
   };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
+    let dataArray = data.outputs[0].data.regions;
+    for(let i in dataArray) {
+      const clarifaiFace =
+      data.outputs[0].data.regions[i].region_info.bounding_box;
     const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
@@ -54,6 +56,7 @@ class App extends Component {
       rightCol: width - clarifaiFace.right_col * width,
       bottomRow: height - clarifaiFace.bottom_row * height,
     };
+    }
   };
 
   displayFaceBox = (box) => {
